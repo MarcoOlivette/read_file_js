2636290c1ac9d7ea195ba37eb6ce30a6b636c37d
Marco Olivette <marco.vieira.olivette@gmail.com>
2024-12-07 07:24:29 -0300

    ✨ important files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# read_file_js
+# Instalação
+
+1. Clonar repositório
+> git clone https://github.com/MarcoOlivette/read_file_js.git
+2. Instalar browser-sync
+> npm install -g browser-sync
+3. Ligar o browser-sync
+> browser-sync start --server --files "*.html, *.css, *.js"
---
+++ main.js
@@ -0,0 +1,10 @@
+// main.js
+
+// Wait for the DOM to fully load
+document.addEventListener('DOMContentLoaded', () => {
+    const messageElement = document.getElementById('message');
+    if (messageElement) {
+      messageElement.textContent = 'JavaScript is working!';
+    }
+  });
+  
